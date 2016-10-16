7aaeb5d70597b1cf152c558a2afd2652c568ce30
zqzqz <fszqz001@sjtu.edu.cn>
2016-10-16 14:57:46 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -8,8 +8,7 @@ from django.db.models.signals import post_save
   
 class UserProfile(models.Model):  
     user = models.OneToOneField(User)  
-    description = models.TextField(max_length=51200)  
-    phoneNum = models.IntegerField(default=100)  
+    description = models.TextField(max_length=51200)   
   
 def create_user_profile(sender, instance, created, **kwargs):  
     if created:  
